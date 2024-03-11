0656fa209f2d05570b00d545b3683d2605cbf1f4
AKANKSHAY9 <84265568+AKANKSHAY9@users.noreply.github.com>
2024-03-11 19:11:21 +0530

    signup complete

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,34 +1,28 @@
+
 const loginBtn = document.querySelector('#login-btn');
-  loginBtn.addEventListener('click', e => {
-  e.preventDefault();
-
-  const email = document.querySelector('#email').value;
-  const password = document.querySelector('#password').value;
-
-  auth.signInWithEmailAndPassword(email, password)
-    .then(cred => {
-      console.log('Logged in user!');
-    })
-    .catch(error => {
-      console.log(error.message);
-    })
-});
-const signupBtn = document.querySelector('#signup-btn');
-    signupBtn.addEventListener('click', e => {
+loginBtn.addEventListener('click', e => {
     e.preventDefault();
 
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
 
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred);
-    console.log('User signed up!');
-  });
+    auth.signInWithEmailAndPassword(email, password)
+        .then(cred => {
+            console.log('Logged in user!');
+            window.location.href = "PAGE1.html";
+        })
+        .catch(error => {
+            console.log(error.message);
+        })
 });
+
+
+
+
 auth.onAuthStateChanged(user => {
     if (user) {
-      console.log(user.email + " is logged in!");
+        console.log(user.email + " is logged in!");
     } else {
-      console.log('User is logged out!');
+        console.log('User is logged out!');
     }
-  });
+});
